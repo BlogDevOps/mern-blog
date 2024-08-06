226f7627f915e92579be786dc554ca9560e972f4
Adrianwachana <adrianwafula403@gmail.com>
2024-08-06 17:57:28 +0300

    added header navbar  functionalities

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Header from './components/Header'; // Assuming you have a Header component
+
 export default function App() {
   return (
-    <h1 className='text-3xl text-yellow-500'>
-      appp
-    </h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </BrowserRouter>
   );
 }
