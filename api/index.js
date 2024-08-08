005d147ec148a397705a60229fedff7fe7778c0e
Adrianwachana <adrianwafula403@gmail.com>
2024-08-08 14:04:39 +0300

    sign in and sign out functionalities,bycrypt and used insomnia as the frontend temporarily

--- api/index.js
+++ api/index.js
@@ -1,14 +1,28 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 dotenv.config();
 
-mongoose.connect(process.env.MONGO)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+const app = express(); // Define the app variable
 
-const app = express();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDB is connected');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 
 app.listen(3000, () => {
-    console.log('server is running on port 3000');
+  console.log('Server is running on port 3000!');
 });
+
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
