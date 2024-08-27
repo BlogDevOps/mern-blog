1f09356d2e15af9b2fb13bf35bbe7b92ed408843
Adrianwachana <adrianwafula403@gmail.com>
2024-08-27 09:01:12 +0300

    implemented dark mode and light mode with theme provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/signin" element={<SignIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
