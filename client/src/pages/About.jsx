05118a7d4eb7fbb9a1700ddeb4fe20c8e0bba362
Adrianwachana <adrianwafula403@gmail.com>
2024-09-02 15:48:03 +0300

    about and database changes

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
-
 export default function About() {
   return (
-    <div>
-      About
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Bearbubbles
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+              Welcome to my blog, a space where I share my thoughts,
+              experiences, and discoveries as I explore the world around us.
+              From the fascinating realms of science and technology to the rich
+              traditions of global cuisine and culture, this blog is my personal
+              journey through the many wonders that capture my interest.
+              Whether I’m writing about the latest developments in
+              sustainability, the art of cooking, or the impact of technology on
+              our lives, my goal is to offer you a fresh perspective and spark
+              curiosity. This is more than just a collection of topics—it's a
+              reflection of the things that inspire me, challenge me, and make
+              me want to dig deeper. I hope you’ll find something here that
+              resonates with you and encourages you to see the world through a
+              new lens.
+            </p>
+            <p>
+              I encourage you to leave comments on my posts and engage with
+              other readers. You can like other people's comments and reply to
+              them as well. 
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
